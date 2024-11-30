7b25dce3351f2d67e15068399a94dc5dc4f007c6
nicolas-thomazini <nicolasfrezarimt@gmail.com>
2024-11-30 19:44:48 -0300

    Initialized the repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A model to predict Moving Avarages of 7, 20 and 50 periods and evaluate model performance.',
+    author='Nicolas Thomazini',
+    license='MIT',
+)
